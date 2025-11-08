64cfeaa956e791cdff874abcfc8a23798c8a7138
Aditya Singh <mnnitadityasingh@gmail.com>
2025-11-08 09:07:25 +0530

    added validation for db url

--- Backend/src/lib/db.js
+++ Backend/src/lib/db.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 import {ENV} from "./env.js"
 export const connectDB = async()=>{
     try {
+        if(!ENV.MONGO_URL){
+            throw new Error("Please provide MONGO_URL in the .env file")
+        }
         const conn = await mongoose.connect(ENV.MONGO_URL);
         console.log("✅Connected to MongoDB:", conn.connection.host);
     } catch (error) {
