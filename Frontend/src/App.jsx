0a3274ffd8d35845e367489cd9285323fcf4c130
Aditya Singh <mnnitadityasingh@gmail.com>
2025-11-15 21:57:20 +0530

    Landing Page Added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,19 @@
 import { useUser } from "@clerk/clerk-react";
-import {Navigate, Routes, Route } from "react-router";
+import { Navigate, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
-
+import DashboardPage from "./pages/DashboardPage";
 import ProblemsPage from "./pages/ProblemsPage";
 import { Toaster } from "react-hot-toast";
 function App() {
-  const { isSignedIn } = useUser();
+  const { isSignedIn, isLoaded } = useUser();
+  //This will get rid of the flickering effect
+  if (!isLoaded) return null;
   return (
     <>
     <Routes>
     {/* <Route path="/" element={<h1 className="text-red-500 bg-orange-400 p-10 text-3xl">Welcome to Vynterview platform</h1>} /> */}
-    <Route path="/" element={<HomePage/>}/>
+    <Route path="/" element={!isSignedIn? <HomePage/> : <Navigate to={"/dashboard"}/>}/>
+    <Route path="/dashboard" element={isSignedIn? <DashboardPage/> : <Navigate to={"/"}/>}/>
     <Route path="/problems" element={isSignedIn? <ProblemsPage/> : <Navigate to={"/"}/>}/>
     </Routes>
     <Toaster toastOptions={{ duration:4000 }}/>
