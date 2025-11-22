9b54cac30e1e87fd11745d6e36481a2a427bf210
Aditya Singh <mnnitadityasingh@gmail.com>
2025-11-22 09:32:53 +0530

    session-page-added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
     <Route path="/dashboard" element={isSignedIn? <DashboardPage/> : <Navigate to={"/"}/>}/>
     <Route path="/problems" element={isSignedIn? <ProblemsPage/> : <Navigate to={"/"}/>}/>
     <Route path="/problem/:id" element={isSignedIn? <ProblemPage/> : <Navigate to={"/"}/>}/>
+    <Route path="/session/:id" element={isSignedIn? <SessionPage/> : <Navigate to={"/"}/>}/>
     </Routes>
     <Toaster toastOptions={{ duration:4000 }}/>
     </>
