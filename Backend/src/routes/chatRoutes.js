38cac55a0afa8225f3ea3a1af5ee8aa5f7914cf6
Aditya Singh <mnnitadityasingh@gmail.com>
2026-01-03 17:22:45 +0530

    updated with extension in chatRoutes

--- Backend/src/routes/chatRoutes.js
+++ Backend/src/routes/chatRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { getStreamToken } from "../controllers/chatController";
-import { protectRoute } from "../middlewares/protectRoute";
+import { getStreamToken } from "../controllers/chatController.js";
+import { protectRoute } from "../middlewares/protectRoute.js";
 const router = express.Router();
 router.get("/token", protectRoute,getStreamToken);
 export default router;
