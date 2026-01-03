a4e2c4c8e288351d3ccc03497673fd9b3f16177d
Aditya Singh <mnnitadityasingh@gmail.com>
2026-01-03 18:14:07 +0530

    path added

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -28,7 +28,7 @@ app.get("/api/health", (req,res)=>{
 //Make our app ready for deployment
 if(ENV.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
-    app.get("/(.*)", (req,res)=>{
+    app.get("/:path(.*)", (req,res)=>{
         res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
     })
 }
