033e1a538fc6bbc03c3795c044e7d27d588fdfa1
Aditya Singh <mnnitadityasingh@gmail.com>
2026-01-03 17:26:33 +0530

    updated extension in sessionRoutes

--- Backend/src/routes/sessionRoutes.js
+++ Backend/src/routes/sessionRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middlewares/protectRoute.js";
-import { createSession, endSession, getActiveSessions, getMyRecentSessions, getSessionById, joinSession } from "../controllers/sessionController";
+import { createSession, endSession, getActiveSessions, getMyRecentSessions, getSessionById, joinSession } from "../controllers/sessionController.js";
 const router = express.Router();
 router.post("/", protectRoute,createSession);
 router.get("/active", protectRoute, getActiveSessions);
