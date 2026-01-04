6a61b20039e7d2231e6f124d69f7a6fa02b24049
Aditya Singh <mnnitadityasingh@gmail.com>
2026-01-04 18:11:34 +0530

    queryClient imported correctly

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,10 +4,8 @@ import './index.css';
 import App from './App.jsx';
 import { ClerkProvider } from '@clerk/clerk-react';
 import { BrowserRouter } from 'react-router';
-import {
-  queryClient,
-  QueryClientProvider
-} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
