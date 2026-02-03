8a5370db2c204b4628cdf0621b66e711c41e6ea4
Aditya Singh <mnnitadityasingh@gmail.com>
2026-02-03 23:59:46 +0530

    removed static file & added debugging script

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,31 +1,38 @@
 import express from "express";
 import cors from "cors";
-import {serve} from "inngest/express";
-import {clerkMiddleware} from "@clerk/express";
-import { ENV } from "./lib/env.js"
+import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
+import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
-import {inngest, functions} from "./lib/inngest.js";
-import { protectRoute } from "./middlewares/protectRoute.js";
+import { inngest, functions } from "./lib/inngest.js";
 import chatRoutes from "./routes/chatRoutes.js";
 import sessionRoutes from "./routes/sessionRoutes.js";
+const PORT = process.env.PORT || 3000;
+
 const app = express();
-import path from "path";
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-//Middleware
+
+// Middleware
 app.use(express.json());
+
+console.log("Allowed Client URL:", ENV.CLIENT_URL);
+
 app.use(cors({
   origin: ENV.CLIENT_URL,
   credentials: true
-})); //Server allows a browser to include cookies on request
-app.use(clerkMiddleware()); //This adds auth field to request object: req.auth()
-app.use("/api/inngest", serve({client:inngest, functions}));
+}));
+
+app.use(clerkMiddleware());
+
+// Routes
+app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
 app.use("/api/sessions", sessionRoutes);
-app.get("/api/health", (req,res)=>{
-    res.status(200).json({message:"API is up and running"})
-})
+
+// Health Check
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ message: "API is up and running" });
+});
+
 app.get("/", (req, res) => {
     res.status(200).json({
         message: "Vynterview API is running successfully",
@@ -36,21 +43,17 @@ app.get("/", (req, res) => {
         }
     });
 });
-//Make our app ready for deployment
-if(ENV.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../../Frontend/dist")));
-    app.get(/.*/, (req,res)=>{
-        res.sendFile(path.join(__dirname, "../../Frontend/dist/index.html"));
-    })
-}
 
-const startServer = async()=>{
+const startServer = async () => {
     try {
         await connectDB();
-        const port = ENV.PORT || 3000;
-        app.listen(port, () => console.log(`✅ Server running on port ${port}`));
+        app.listen(PORT, () => {
+            console.log(`✅ Server running on port ${PORT}`);
+            console.log(`✅ CORS allowing: ${ENV.CLIENT_URL}`);
+        });
     } catch (error) {
-        console.error("💥Error starting the server:", error)
+        console.error("💥 Error starting the server:", error);
     }
 };
+
 startServer();
